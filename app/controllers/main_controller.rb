db4af1886a10fa56476557dde09b6f4209306180
RyumaNoma <doragon.ryu-ma@ezweb.ne.jp>
2019-07-06 21:25:14 +0900

    no count

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,9 @@
 class MainController < ApplicationController
   def lp
-    app = App.find(1)
-    num = app.exp[/\d/].to_i
-    num += 1
-    app.exp = num.to_s
-    app.save
+    # app = App.find(1)
+    # num = app.exp[/(\d)+/].to_i
+    # num += 1
+    # app.exp = num.to_s
+    # app.save
   end
 end
